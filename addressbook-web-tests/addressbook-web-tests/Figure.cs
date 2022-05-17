3fadfa6e4b95fadacf0a07ee9972fdef38460e73
Professional <dima02ufa@yandex.ru>
2022-05-17 17:42:57 +0500

    Лекция 1.5

---
+++ addressbook-web-tests/addressbook-web-tests/Figure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    public class Figure
+    {
+        public bool colored = false;
+
+        public bool Colored
+        {
+            get
+            {
+                return colored;
+            }
+            set
+            {
+                Colored = value;
+            }
+        }
+    }
+}
