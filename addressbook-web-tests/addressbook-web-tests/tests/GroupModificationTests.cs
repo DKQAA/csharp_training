e65d5fb1ac14d93cfbd7f25ff8afce5653392265
Professional <dima02ufa@yandex.ru>
2022-05-19 12:41:46 +0500

    Лекция 2.5

---
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupModificationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace WebAddressbookTests.test
+{
+    [TestFixture]
+    public class GroupModificationTests : TestBase
+    {
+        [Test]
+        public void GroupModificationTest()
+        {
+
+
+            GroupData newData = new GroupData("Cat");
+            newData.Header = "Dog";
+            newData.Footer = "Fish";
+
+
+            app.Groups.Modify(1, newData);
+
+        }
+
+
+    }
+}
