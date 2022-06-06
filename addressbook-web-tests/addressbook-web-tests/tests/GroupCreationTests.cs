dcb2b2cd97dad94d418f805384f88fed4a06e80f
Professional <Professional@DESKTOP-QVKUDVQ>
2022-06-06 11:04:27 +0500

    Лекция 7.1

--- addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/tests/GroupCreationTests.cs
@@ -9,6 +9,8 @@ using System.Xml;
 using System.Xml.Serialization;
 using Excel = Microsoft.Office.Interop.Excel;
 using Newtonsoft.Json;
+using System.Linq;
+
 
 namespace WebAddressbookTests
 {
@@ -127,6 +129,20 @@ namespace WebAddressbookTests
             newGroups.Sort();
             Assert.AreEqual(oldGroups, newGroups);
 
+        }
+
+        [Test]
+        public void TestDBConnectivity()
+        {
+            DateTime start = DateTime.Now;
+            List<GroupData> fromUi = app.Groups.GetGroupList();
+            DateTime end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
+
+            start = DateTime.Now;
+            List<GroupData> fromDb = GroupData.GetAll();
+            end = DateTime.Now;
+            System.Console.Out.WriteLine(end.Subtract(start));
         }
     }
 }
