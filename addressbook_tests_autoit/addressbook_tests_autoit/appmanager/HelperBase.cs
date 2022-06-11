d6d2ad4b6b6fed61d4131e6728d42207dce047a0
Professional <Professional@DESKTOP-QVKUDVQ>
2022-06-11 20:10:51 +0500

    Лекция 8.1.

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/HelperBase.cs
@@ -0,0 +1,23 @@
+﻿using AutoItX3Lib;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+ 
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+        protected string WINTITLE;
+        protected AutoItX3 aux;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+            this.aux = manager.Aux;
+            WINTITLE = ApplicationManager.WINTITLE;
+            
+        }
+    }
+}
